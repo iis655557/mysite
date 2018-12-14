f24d01518922dab2ab62bee764c46a733e588bd5
ma_jing <2220766092@qq.com>
2018-12-14 10:06:28 +0800

    表创建前

---
+++ polls/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from . import views
+urlpatterns=[
+    path('',views.index,name='index'),
+    path('index',views.index,name='index')
+]
+
+
+#先引入视图函数
+#path()函数定义的路由最终都会在项目启动时加载
+#path（路由规则，）
