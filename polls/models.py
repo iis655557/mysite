8136c203492134c583aaac40ec3bc97ffcd0ecad
ma_jing <2220766092@qq.com>
2018-12-17 12:06:58 +0800

    表创建前

--- polls/models.py
+++ polls/models.py
@@ -20,13 +20,25 @@ create table votes if not exists(
 #django自带orm框架，用法类似sqlalchemy
 #自定义的类要继承orm框架中的Model类，这样orm框架能把类和数据库联系起来。
 from django.db import models
+from django.utils import timezone
+from datetime import  datetime,timedelta
 
 # Create your models here.
 class Question(models.Model):
     question_text=models.CharField('问题内容',max_length=200)
-    pub_data=models.DateField('发布时间')
+    pub_date=models.DateTimeField('发布时间')
+    def __str__(self):     #（了解）控制打印对象时输出的信息Q
+        return self.question_text
+
+    def was_published_recently(self):
+        if timezone.now() - timedelta(days=1) <= self.pub_date:
+            return True
+        else:
+            return False
 
 class Choice(models.Model):
     question=models.ForeignKey(Question,on_delete=models.CASCADE)
     choice_text=models.CharField(max_length=200)
     votes=models.IntegerField(default=0)
+    def __str__(self):
+        return self.choice_text
